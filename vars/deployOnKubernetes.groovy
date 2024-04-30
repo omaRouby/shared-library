44b244bd1307499f0a0e954c37c31651c3b0c262
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-05-01 01:15:46 +0300

    Update deployOnKubernetes.groovy

--- vars/deployOnKubernetes.groovy
+++ vars/deployOnKubernetes.groovy
@@ -8,8 +8,6 @@ def call(String k8sCredentialsID, String imageName) {
 
    // login to k8s Cluster via KubeConfig file
     withCredentials([file(credentialsId: "${k8sCredentialsID}", variable: 'KUBECONFIG_FILE')]) {
-        sh "export KUBECONFIG=${KUBECONFIG_FILE} && kubectl apply -f deployment.yaml"
+        sh "export KUBECONFIG=${KUBECONFIG_FILE} && kubectl create namespace $BRANCH_NAME && kubectl config set-context --current --namespace=$BRANCH_NAME && kubectl apply -f deployment.yaml"
     }
 }
-
-
