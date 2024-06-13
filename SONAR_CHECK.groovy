957ef4ed442cb0e7bafa427f2cf08624977a9ef1
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-06-13 03:41:45 +0300

    Update and rename SONAR_CHECK.groovy to README.md

--- SONAR_CHECK.groovy
+++
@@ -1,9 +0,0 @@
-#!usr/bin/env groovy
-def call(){ 
-	echo "Running SonarQube "
-	withSonarQubeEnv(credentialsId: 'sonar') {
-		echo "Running SonarQube Analysis..."
-		sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=MULTICLOUD \
-		-Dsonar.java.binaries=. \
-		-Dsonar.projectKey=MULTICLOUD '''
-	}
